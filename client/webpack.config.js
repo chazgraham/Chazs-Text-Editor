c8a10217e1e4037cb7323f6d2ff31e1f9a1292c7
chaz Graham <chazhg80.cg@gmail.com>
2022-11-23 01:22:39 -0600

    added cahche to service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 module.exports = {
   mode: 'production',
   entry: './src/js/index.js',
@@ -41,54 +40,24 @@ module.exports = {
       title: 'Webpack Plugin',
     }),
     new InjectManifest({
-      swSrc: './src/sw.js',
+      swSrc: './src-sw.js',
       swDest: 'service-worker.js',
     }),
     new WebpackPwaManifest({
-      name: 'Contact Cards Application',
-      short_name: 'Contact Cards',
-      description: 'Keep track of contacts!',
+      name: 'Just Another Text Editor',
+      short_name: 'jane',
+      description: 'Just Another Text Editor!',
       background_color: '#7eb4e2',
       theme_color: '#7eb4e2',
       start_url: './',
       publicPath: './',
       icons: [
         {
-          src: path.resolve('src/images/icon-manifest.png'),
+          src: path.resolve('src/images/logo.png'),
           sizes: [96, 128, 192, 256, 384, 512],
           destination: path.join('assets', 'icons'),
-        },
-        {
-          src: path.resolve('src/images/icon-manifest.png'),
-          size: '1024x1024',
-          destination: path.join('assets', 'icons'),
-          purpose: 'maskable'
         }
       ],
     }),
   ]
 }
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
-};
