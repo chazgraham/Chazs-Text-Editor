93b5b5d7e3e6f0383f3951e8b419f713e545f4f0
chaz Graham <chazhg80.cg@gmail.com>
2022-11-23 12:21:45 -0600

    added database routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -10,12 +10,50 @@ const initdb = async () =>
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
-  });
+});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Add to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .put() method to store all data in the database.
+  const request = store.put({ id: 1, value: content });
+  console.log(request);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  return result;
+}
+
+// Export a function we will use to GET to the database.
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
       title: 'Webpack Plugin',
     }),
     new InjectManifest({
-      swSrc: './src-sw.js',
+      swSrc: './src/src-sw.js',
       swDest: 'service-worker.js',
     }),
     new WebpackPwaManifest({
